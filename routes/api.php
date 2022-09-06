fd4bcffe39c34bc772f63a3f22c6542024134d97
AldemiroAlldevelopers <90455690+AldemiroAntonio@users.noreply.github.com>
2022-09-06 15:36:54 +0100

    Feat create routes
    Criando as rotas da api

--- routes/api.php
+++ routes/api.php
@@ -3,17 +3,30 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ContactController;
+
 Route::get("/ping", function(){
     return ["/pong"=>true];
 });
+
+Route::post("/auth/login", [AuthController::class, 'login']);
+Route::post("/auth/logout", [AuthController::class, 'logout']);
+Route::post("/auth/refresh", [AuthController::class, 'refresh']);
+Route::post('/user', [AuthController::class, 'create']);
+
+Route::get('/user', [UserController::class, 'read']);
+Route::get("/user/favorites", [UserController::class, 'getFavorites']);
+Route::post("/user/favorites", [UserController::class, 'addFavorite']);
+Route::get('/users', [UserController::class, 'list']);
+
+Route::post("/contact", [ContactController::class, 'create']);
+Route::get("/contacts", [ContactController::class, 'list']);
+Route::put("/contact", [ContactController::class, 'update']);
+Route::get("/search", [ContactController::class, 'search']);
+
+
+
+
 
